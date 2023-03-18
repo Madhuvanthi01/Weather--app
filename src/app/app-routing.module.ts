c84fe478021ad01e77c37eaacb4716af361ab9b8
Madhuvanthi01 <madhuravi2k2@gmail.com>
2023-03-18 11:21:31 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
-  {path:'dashboad',component:DashboardComponent},
+  {path:'dashboard',component:DashboardComponent},
 
 
 ];
